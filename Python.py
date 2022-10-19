eb6172061466f439397902f491a08e799e0375d3
sudalS2 <98267987+sudalS2@users.noreply.github.com>
2022-10-19 21:43:58 +0900

    Update Python.py

---
+++ Python.py
@@ -0,0 +1,77 @@
+# 프로그래머스 코딩 Test 입문
+
+## 두 수의 합
+
+### 1.
+def solution(num1, num2):
+    answer = num1 + num2
+    return answer
+
+### 2.
+solution = lambda num1, num2 : num1 + num2
+
+## 두 수의 차
+
+### 1.
+def solution(num1, num2):
+    answer = num1 - num2
+    return answer
+
+### 2.
+solution = lambda num1, num2 : num1 - num2
+
+## 두 수의 곱
+
+### 1.
+def solution(num1, num2):
+    answer = num1 * num2
+    return answer
+
+### 2.
+solution = lambda num1, num2 : num1 * num2
+
+## 두 수의 나눗셈
+
+### 1.
+def solution(num1, num2):
+    answer = num1 / num2
+    return answer
+
+### 2.
+solution = lambda num1, num2 : num1 / num2
+
+## 몫 구하기
+
+### 1.
+def solution(num1, num2):
+    answer = num1 // num2
+    return answer
+
+### 2.
+solution = lambda num1, num2 : num1 // num2
+
+## 나머지 구하기
+
+### 1.
+def solution(num1, num2):
+    answer = num1 % num2
+    return answer
+
+### 2.
+solution = lambda num1, num2 : num1 % num2
+
+## 숫자 비교하기
+
+### 1.
+def solution(num1, num2):
+    if num1 == num2:
+        return 1
+    else:
+        return -1
+
+### 2.
+def solution(num1, num2):
+    return 1 if num1==num2 else -1
+
+## 분수의 덧셈
+
