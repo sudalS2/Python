dfd2559679ba4626540136892aac337d02a4a81a
sudalS2 <98267987+sudalS2@users.noreply.github.com>
2022-10-29 15:47:31 +0900

    Update Python.py

--- Python.py
+++ Python.py
@@ -532,3 +532,44 @@ def solution(age):
     for i in range(0, len(age_list)):
         answer += PG_AGE[age_list[i]]
     return answer
+
+## 진료 순서 정하기
+
+### 1.
+def solution(emergency):
+    answer = []
+    desc_eg= sorted(emergency,reverse = True)
+
+    for i in emergency:
+        answer.append(desc_eg.index(i)+1)
+    return answer
+
+### 2.
+def solution(emergency):
+    return [sorted(emergency, reverse=True).index(i) + 1 for i in emergency]
+
+## 순서쌍의 개수
+
+### 1.
+def solution(n):
+    answer= []
+    for i in range(1, n+1):
+        if n % i == 0:
+            answer.append([i, n // i])
+    return len(answer)
+
+### 2.
+def solution(n):
+    answer =0 
+    for i in range(n):
+        if n % (i+1) ==0:
+            answer +=1
+    return answer
+
+### 3.
+def solution(n):
+    return len(list(filter(lambda v: n % (v+1) == 0, range(n))))
+
+### 4. 
+def solution(n):
+    return len([i for i in range(1, n+1) if n % i == 0])
