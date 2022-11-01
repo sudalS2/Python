d6922dc25281be34afbef6ec7686801aebf63902
sudalS2 <98267987+sudalS2@users.noreply.github.com>
2022-11-01 19:30:32 +0900

    Update 프로그래머스_고득점kit.py

--- 프로그래머스_고득점kit.py
+++ 프로그래머스_고득점kit.py
@@ -5,11 +5,20 @@
 ### 1.
 import collections
 
-
 def solution(participant, completion):
     answer = collections.Counter(participant) - collections.Counter(completion)
     return list(answer.keys())[0]
-    
+
+### 설명
+counter["l"] += 1 -> 원소 카운트 1개 더하기
+counter["h"] -= 1 -> 원소 카운트 1개 빼기
+
+ex) Counter('hellow world') -> Counter({'h': 0, 'e': 1, 'l': 4, 'o': 2, ' ': 1, 'w': 1, 'r': 1, 'd': 1})
+
+most_common(): 데이터의 개수가 많은 순으로 정렬된 배열을 리턴
+ex) Counter('hello world').most_common() -> [('l', 3), ('o', 2), ('h', 1), ('e', 1), (' ', 1), ('w', 1), ('r', 1), ('d', 1)]
+ex) Counter('hello world').most_common(1) -> [('l', 3)]
+
 ### 2.
 def solution(participant, completion):
     answer = ''
@@ -19,7 +28,7 @@ def solution(participant, completion):
         dic[hash(part)] = part
         temp += int(hash(part))
     for com in completion:
-        temp -= hash(com)
+        temp -= int(hash(com))
     answer = dic[temp]
 
     return answer
@@ -42,4 +51,93 @@ def solution(participant, completion):
             return p
     return participant[-1]
   
+## 폰켓몬
+
+### 1.
+### 2.
+### 3.
+
+## 폰켓몬
+
+### 1.
+### 2.
+### 3.
+
+## 폰켓몬
+
+### 1.
+### 2.
+### 3.
   
+## 폰켓몬
+
+### 1.
+### 2.
+### 3.
+
+## 폰켓몬
+
+### 1.
+### 2.
+### 3.
+
+## 폰켓몬
+
+### 1.
+### 2.
+### 3.
+
+## 폰켓몬
+
+### 1.
+### 2.
+### 3.
+
+## 폰켓몬
+
+### 1.
+### 2.
+### 3.
+
+## 폰켓몬
+
+### 1.
+### 2.
+### 3.
+
+## 폰켓몬
+
+### 1.
+### 2.
+### 3.
+
+## 폰켓몬
+
+### 1.
+### 2.
+### 3.
+
+## 폰켓몬
+
+### 1.
+### 2.
+### 3.
+
+## 폰켓몬
+
+### 1.
+### 2.
+### 3.
+
+## 폰켓몬
+
+### 1.
+### 2.
+### 3.
+
+## 폰켓몬
+
+### 1.
+### 2.
+### 3.
+
